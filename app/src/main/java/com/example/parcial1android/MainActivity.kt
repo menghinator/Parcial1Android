6ccd53c5c5b7a065eb8eb84c299481461a92b85f
menghinator <tlopezmenghi@gmail.com>
2023-10-09 16:16:16 -0300

    added a sample Receta for testing and the implementation of the adapter on the Recycler

--- app/src/main/java/com/example/parcial1android/MainActivity.kt
+++ app/src/main/java/com/example/parcial1android/MainActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-private lateinit var rvReceta: RecyclerView
-private lateinit var adapter: Adapter
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvReceta: RecyclerView
+    private lateinit var adapter: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         rvReceta.layoutManager = LinearLayoutManager(this)
         adapter = Adapter(this)
         rvReceta.adapter = adapter
+        adapter.submitList(getListadoRecetas())
     }
+
+    private fun getListadoRecetas(): MutableList<Receta>{
+    return mutableListOf(
+        Receta(1,"Test",Pais.ARGENTINA, listOf("ingrediente1","ingrediente2"),Dificultad.PRINCIPIANTE,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7MCydoToJUHZmVoPRbZ2XfVH0dnqo9wqUfg&usqp=CAU")
+    )
+    }
+
 }
 
